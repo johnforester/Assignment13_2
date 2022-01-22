32eb6962f7b8034830d8a4852f0eefb0751be732
John Forester <forester.john@gmail.com>
2022-01-22 18:24:37 -0500

    Add unity files

---
+++ Week13/Assets/DrumMachineUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumMachineUI : MonoBehaviour
+{
+    [SerializeField] List<GameObject> BeatMarkers = new List<GameObject>();
+    
+
+    private int m_currentBeat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject beatMarker in BeatMarkers)
+        {
+            beatMarker.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BeatChanged(int beat)
+    {
+        BeatMarkers[m_currentBeat].SetActive(false);
+        m_currentBeat = beat;
+        BeatMarkers[m_currentBeat].SetActive(true);
+    }
+}
